a21b463978273b297dab530542de12819bb66b2e
YurySE <chuksin.yury@gmail.com>
2022-10-12 11:40:14 +0200

    Start - initial code

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+def check_relation(net: set, first: str, second: str) -> bool:
+    pass
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    net = (
+        ("Ваня", "Лёша"), ("Лёша", "Катя"),
+        ("Ваня", "Катя"), ("Вова", "Катя"),
+        ("Лёша", "Лена"), ("Оля", "Петя"),
+        ("Стёпа", "Оля"), ("Оля", "Настя"),
+        ("Настя", "Дима"), ("Дима", "Маша")
+    )
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    assert check_relation(net, "Петя", "Стёпа") is True
+    assert check_relation(net, "Маша", "Петя") is True
+    assert check_relation(net, "Ваня", "Дима") is False
+    assert check_relation(net, "Лёша", "Настя") is False
+    assert check_relation(net, "Стёпа", "Маша") is True
+    assert check_relation(net, "Лена", "Маша") is False
+    assert check_relation(net, "Вова", "Лена") is True
